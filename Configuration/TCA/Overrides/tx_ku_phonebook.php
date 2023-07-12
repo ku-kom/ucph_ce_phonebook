859bb8e1176f503adef77287ab0ca13546a25d9a
Nanna Ellegaard <nel@adm.ku.dk>
2023-07-12 14:02:19 +0200

    Setup

--- Configuration/TCA/Overrides/tx_ku_phonebook.php
+++ Configuration/TCA/Overrides/tx_ku_phonebook.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the package ku_phonebook.
+ * This file is part of the package ucph_ce_phonebook.
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
  * Sep 2022 Nanna Ellegaard, University of Copenhagen.
